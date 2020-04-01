6cd628487c251c9796593c7ef8ad556e8cc96db2
GanxtaMo <mokrane_yahiatene@web.de>
2020-04-01 21:31:45 +0200

    first read in file

--- src/main/java/esiCode/EsiApp.java
+++ src/main/java/esiCode/EsiApp.java
@@ -1,7 +1,16 @@
 package esiCode;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class EsiApp {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+
+        System.out.println(System.getProperty("user.dir"));
+        File file = new File(System.getProperty("user.dir")+"/src/main/resources/Test.png");
+        BufferedImage image = ImageIO.read(file);
     }
 }
