3779c649722d5ea2a5c670b37b471d10ae97d4b4
GanxtaMo <mokrane_yahiatene@web.de>
2020-04-01 22:21:23 +0200

    first reading of tiff file and implementation

--- src/main/java/esiCode/EsiApp.java
+++ src/main/java/esiCode/EsiApp.java
@@ -1,16 +1,34 @@
 package esiCode;
 
 import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.ImageInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
 
 public class EsiApp {
-
+    /**
+     * @param args
+     * @throws IOException
+     */
     public static void main(String[] args) throws IOException {
 
-        System.out.println(System.getProperty("user.dir"));
-        File file = new File(System.getProperty("user.dir")+"/src/main/resources/Test.png");
-        BufferedImage image = ImageIO.read(file);
+        File file = new File(System.getProperty("user.dir") + "/src/main/resources/QD655.tif");
+        ImageInputStream is = ImageIO.createImageInputStream(file);
+        if (is == null | is.length() == 0) {
+            throw new IOException("Error");
+        }
+        Iterator<ImageReader> iterator = ImageIO.getImageReaders(is);
+        if (iterator == null || !iterator.hasNext()) {
+            throw new IOException("Image file format not supported by ImageIO: " + file.getAbsolutePath());
+        }
+        // We are just looking for the first reader compatible:
+        ImageReader reader = iterator.next();
+        iterator = null;
+        reader.setInput(is);
+        int pageNum = reader.getNumImages(true);
+        System.out.println("Anzahl der Pages des TiffImages: " + pageNum + " Bilder\n");
+        //reader.read(Index der Seite) // Source: https://stackoverflow.com/questions/17770071/splitting-a-multipage-tiff-image-into-individual-images-java
     }
 }
