6d8618ea68c8e5d60c2e1b1c0bad5c0f36ba9046
GanxtaMo <mokrane_yahiatene@web.de>
2020-04-02 12:14:40 +0200

    first ImageReaderESI Test

--- src/main/java/ESIApp.java
+++ src/main/java/ESIApp.java
@@ -1,6 +1,11 @@
+import model.ImageReaderESI;
+
+import java.io.IOException;
+
 public class ESIApp {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws IOException {
+        ImageReaderESI readerESI = new ImageReaderESI("/src/main/resources/QD655.tif");
+        readerESI.read();
     }
 }
