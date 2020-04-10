1d8652fbe4ef1837de7ec7e9198aa11a8e5fcd65
Idir <idirnrwbi@gmail.com>
2020-04-10 21:13:22 +0200

    implemented temporary method "writePixel" to set pixel values of the new (reconstructed) image.

--- src/main/java/model/ImageWriterESI.java
+++ src/main/java/model/ImageWriterESI.java
@@ -1,4 +1,35 @@
 package model;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 public class ImageWriterESI {
+
+    private String path;
+    private javax.imageio.ImageWriter writer; //ImageReader read set as global
+
+    public ImageWriterESI(final String path) throws IOException {
+        this.path = path;
+    }
+
+
+    public void writePixel(int x, int y, double value) {
+        BufferedImage theImage = new BufferedImage(x, y,
+            BufferedImage.TYPE_BYTE_BINARY);
+
+        theImage.setRGB(x, y, (int) (value));
+
+        File outputfile = new File(path);
+        try {
+            ImageIO.write(theImage, "png", outputfile);
+        } catch (IOException e1) {
+            System.out.println("Unable to write file: " + e1.getStackTrace());
+
+        }
+
+    }
+
+
 }
