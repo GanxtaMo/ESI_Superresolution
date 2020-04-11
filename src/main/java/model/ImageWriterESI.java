9d0cd1d212c110429f84ae556cac0e330d343fd9
Idir <idirnrwbi@gmail.com>
2020-04-11 10:51:12 +0200

    removed changes

--- src/main/java/model/ImageWriterESI.java
+++ src/main/java/model/ImageWriterESI.java
@@ -1,34 +1,6 @@
 package model;
 
-import javax.imageio.ImageIO;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-
 public class ImageWriterESI {
-
-    private String path;
-
-    public ImageWriterESI(final String path) throws IOException {
-        this.path = path;
-    }
-
-
-    public void writePixel(int x, int y, double value) {
-        BufferedImage theImage = new BufferedImage(x, y,
-            BufferedImage.TYPE_BYTE_GRAY);
-
-        theImage.setRGB(x, y, (int) (value));
-
-        File outputfile = new File(path);
-        try {
-            ImageIO.write(theImage, "png", outputfile);
-        } catch (IOException e1) {
-            System.out.println("Unable to write file: " + e1.getStackTrace());
-
-        }
-
-    }
 
 
 }
